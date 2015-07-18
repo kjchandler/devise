becaf4b88240d9590ac09ff4e9a54570cfa91e3a
Kelly Chandler <chandlerkelly@hotmail.com>
2015-07-18 13:05:36 -0400

    Devise/Instagram

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_action :check_profiles_exists
+
+  protected
+
+  def check_profile_exists
+  	if user_signed_in? && !current_user.profile
+  		redirect_to new_profile_path
+  	end
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,61 @@
+class PostsController < ApplicationController
+		before_action :authenticate_user!, only: [:destroy]
+		before_action :set_post, only: [:show, :destroy]
+		before_action :require_auth, only: [:destroy]
+
+		def new
+			@post = Post.new
+		end
+
+		def show
+			
+		end
+
+		def create
+			@post = Post.new(post_params)
+			@post.user_id = current_user.id
+
+			respond_to do |format|
+				if(@post.save)
+					format.html { redirect_to @post }
+				else
+					format.html { render :new}
+				end
+			end
+		end
+
+		def destroy
+			@post.destroy
+			respond_to do |format|
+				format.html { redirect_to '/', :notice => 'Post deleted successfully.' }
+		end	
+	end
+
+private
+
+def require_auth
+	if @post.user_id != current_user.id
+		flash[:alert] = "You are not authorized to perform that action"
+		redirect_ :back
+	end
+end
+
+def set_post
+	@post = Post.find(params[:id])
+	
+end
+
+def post_params
+	params.require(:post).permit(:description, :picture)
+	
+end
+
+
+
+
+
+
+
+
+
+
